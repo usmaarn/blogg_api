27bb3eff36419f1fbc4002d16217afed4c32a234
Usmaarn <Usmaarn@gmail.com>
2025-05-25 03:44:45 +0100

    switching to chi

---
+++ cmd/database/migrations/20250524175724_create_users_table.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users(
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  username VARCHAR(255),
+  email VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  role VARCHAR(10) NOT NULL,
+  status VARCHAR(10) NOT NULL,
+  password TEXT NOT NULL,
+
+  CONSTRAINT unique_users_email UNIQUE (email),
+  CONSTRAINT unique_users_username UNIQUE (username),
+  CONSTRAINT unique_users_phone UNIQUE (phone_number)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/usmaarn/blogg_api
 
 go 1.24.1
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/lib/pq v1.10.9
+	github.com/pressly/goose/v3 v3.24.3
+)
+
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+)
